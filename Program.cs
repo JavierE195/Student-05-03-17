140a875619cedf65c596c7e05676699e4fa92eb4
JavierE195 <ellerbrockjavier@hotmail.com>
2017-05-03 14:33:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the student's name: ");
+            string name01 = Console.ReadLine();
+
+            Console.WriteLine("Insert the student's GPA: ");
+            double gpa01 = double.Parse(Console.ReadLine());
+
+            Student student01 = new Student();
+            student01.name = name01;
+            student01.gpa = gpa01;
+
+            Console.Write(
+                "Student: " + student01.name +
+                "\nGPA: " + student01.gpa);
+            Console.ReadKey();
+        }
+    }
+}
